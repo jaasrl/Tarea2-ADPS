a5b44ba655636c83c25e6e7c4bfa62cf745b5e47
Ingrid Serrano <146678484+ingrid-jaz-021@users.noreply.github.com>
2024-02-11 16:41:36 -0600

    Update prueba.c
    Agregué un comentario de lo que se realiza en la línea 4.

--- prueba.c
+++ prueba.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-printf("Hola mundo\n");
+printf("Hola mundo\n"); // Muestra en pantalla el enunciado "Hola mundo", y hace un salto de linea con "\n".
 return 0;
 }
