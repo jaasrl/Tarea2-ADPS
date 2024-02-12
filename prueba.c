35e1f730a533b9055876d7f6242a76e06cc1c131
zbdielinho <72850076+zbdielinho@users.noreply.github.com>
2024-02-11 23:05:49 -0600

    Update prueba.c
    una funcion del stdlib que me acuerdo

--- prueba.c
+++ prueba.c
@@ -1,8 +1,10 @@
 //Tarea 2 Equipo 1
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+system("color 3b"); //modifica el color de visualizacion de la pantalla emergente del programa
 printf("Hola mundo\n"); // Muestra en pantalla el enunciado "Hola mundo", y hace un salto de linea con "\n".
 return 0;
 }
