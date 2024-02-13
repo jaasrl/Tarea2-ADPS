3a0fd33071724c2e266a4b2d61f07a3b78befde4
DeLosSantosKarlos <90476877+DeLosSantosKarlos@users.noreply.github.com>
2024-02-12 19:38:58 -0600

    Update prueba.c
    Lo siento. Se me olvido:((

--- prueba.c
+++ prueba.c
@@ -4,7 +4,11 @@
 #include <stdlib.h>
 
 int main(){
-system("color 3b"); //modifica el color de visualizacion de la pantalla emergente del programa
-printf("Hola mundo\n"); // Muestra en pantalla el enunciado "Hola mundo", y hace un salto de linea con "\n".
+  
+  printf("=====================");
+  printf("| Tarea 2 | Equipo 1 | 
+  system("color 3b"); //modifica el color de visualizacion de la pantalla emergente del programa
+  printf("Hola mundo\n"); // Muestra en pantalla el enunciado "Hola mundo", y hace un salto de linea con "\n".
+  printf("=====================");
 return 0;
 }
