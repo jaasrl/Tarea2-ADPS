cc9186102c29577461aba8d9e5768d6a94e23bc4
DeLosSantosKarlos <90476877+DeLosSantosKarlos@users.noreply.github.com>
2024-02-12 19:42:00 -0600

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -6,7 +6,7 @@
 int main(){
   
   printf("=====================");
-  printf("| Tarea 2 | Equipo 1 | 
+  printf("| Tarea 2 | Equipo 1 |");
   system("color 3b"); //modifica el color de visualizacion de la pantalla emergente del programa
   printf("Hola mundo\n"); // Muestra en pantalla el enunciado "Hola mundo", y hace un salto de linea con "\n".
   printf("=====================");
